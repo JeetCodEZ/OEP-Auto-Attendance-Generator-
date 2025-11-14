c1dbafab092aaabc2c1818bea2f5ec1b0443fead
JeetCodEZ <jeetvpatel06@gmail.com>
2025-11-14 19:50:27 +0530

    Update ALLOWED_HOSTS for Render

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y4r*tg8rz7y4o*4xcozp#bpqbvr8_thsn%mdba5r%6746fk)k6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'realm-automatic-attendance-generator.onrender.com',
+    'localhost',    # Good to keep for local testing
+    '127.0.0.1',  # Good to keep for local testing
+]
 
 
 # Application definition
